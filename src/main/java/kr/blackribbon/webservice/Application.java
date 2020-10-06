14bcfdbce0a7bf37925afcfe1d6369e24207ad07
Jinyong Kim <knowhow999@gmail.com>
2020-10-06 13:46:13 +0900

    default setup

---
+++ src/main/java/kr/blackribbon/webservice/Application.java
@@ -0,0 +1,13 @@
+package kr.blackribbon.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+}
